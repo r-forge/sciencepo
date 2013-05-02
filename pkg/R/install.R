3016127c73fa2fa7efa6417681c6aae4bde10bc3
dmarcelino (Daniel Marcelino) <dmarcelino@live.com>
2013-05-02 19:27:26 +0000

    Removing install function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciencepo@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/install.R
+++
@@ -1,6 +0,0 @@
-install <-
-function(packages=NULL, install=TRUE) {
-
-if(install){install.packages(packages, dependencies=TRUE, repos = "http://cran.r-project.org")}
-lapply(packages, library, character.only = TRUE)
-}
